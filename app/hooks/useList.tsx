1a4c85faaabe180001e85f06ba25300ff6cc5708
Shirley Park <shirley.park7@gmail.com>
2023-07-20 20:46:36 +1200

    add market list state as a hook

---
+++ app/hooks/useList.tsx
@@ -0,0 +1,31 @@
+import { create } from 'zustand'
+
+export type Item = {
+  id: number
+  name: string
+}
+
+type Items = Array<Item>
+
+interface listState {
+  items: Items
+}
+
+const mockData = [
+  {
+    id: 1,
+    name: 'apple',
+  },
+  {
+    id: 2,
+    name: 'orange',
+  },
+  {
+    id: 3,
+    name: 'banana',
+  },
+]
+
+export const useList = create<listState>((set) => ({
+  items: [...mockData],
+}))
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+'use client'
+import { useList } from './hooks/useList'
+
 export default function Home() {
+  const list = useList((state) => state.items)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 flex-col text-center lg:mb-0 lg:text-left">
@@ -7,15 +12,14 @@ export default function Home() {
         </h1>
 
         <ul className="text-md opacity-70">
-          <li className="border-solid border-2 border-gray-400 rounded-md mb-4">
-            apple
-          </li>
-          <li className="border-solid border-2 border-gray-400 rounded-md mb-4">
-            orange
-          </li>
-          <li className="border-solid border-2 border-gray-400 rounded-md mb-4">
-            banana
-          </li>
+          {list.map((item) => (
+            <li
+              key={item.id}
+              className="border-solid border-2 border-gray-400 rounded-md mb-4"
+            >
+              {item.name}
+            </li>
+          ))}
         </ul>
       </div>
     </main>
